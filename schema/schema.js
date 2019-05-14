3895e798a79aa8284b57a58c597eb727233a2160
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-05-13 17:19:24 -0700

    install json-server

--- schema/schema.js
+++ schema/schema.js
@@ -58,6 +58,22 @@ const UserType = new GraphQLObjectType({
 //read the following as : "You can ask me, the Root Query about Users in the application"
 // ..if you give me the ID of the user you are looking for, I will return a user back to you
 //ROOT query has expectation of getting the id
+//THIS QUERY GETS SENT TO OUR ROOTQUERYTYPE
+  //1) it then takes that query and enters into our graph of data
+  //2) BC we specified USER as the field of the query, the root query went and found the
+  //user key inside of its fields object
+  //3) we specified in that fields object that the query should come with an id that is a type String
+  //4) Graph ql then plucks off the object returned in the resolve function,
+   //   a) the id, firstname, and age
+//{
+//   user(id : "23") {
+//     id
+//     firstName
+//     age
+//   }
+// }
+
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -66,6 +82,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } }, // 1) if you give me this
       //arguments required for this root query
       resolve(parentValue, args) {
+        //return raw json here and graphql takes care of the rest
         return _.find(users, { id: args.id });
       }
     }
@@ -86,3 +103,5 @@ module.exports = new GraphQLSchema({
 //so resolve function's job is to go out and get that real data
 // parentValue is bullshit, args, however is an object that gets called with whatever arguments
 //. . . where passed into the ORIGINAL QUERY
+
+//our browser is running graphiql and it is sending a graphql query to our express server
